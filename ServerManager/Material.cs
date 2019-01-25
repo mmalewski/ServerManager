3361ed8c4ba6eefd4b5b1c7d558546237e1b74b5
Hannele Ruiz <justlemoncl@gmail.com>
2019-01-24 23:53:04 -0300

    Added orange color scheme

--- ServerManager/Material.cs
+++ ServerManager/Material.cs
@@ -1,4 +1,5 @@
-﻿using MaterialSkin.Controls;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
 
 namespace ServerManager
 {
@@ -7,6 +8,11 @@ namespace ServerManager
         public Material()
         {
             InitializeComponent();
+
+            MaterialSkinManager manager = MaterialSkinManager.Instance;
+            manager.AddFormToManage(this);
+            manager.Theme = MaterialSkinManager.Themes.LIGHT;
+            manager.ColorScheme = new ColorScheme(Primary.Orange800, Primary.Orange900, Primary.Orange500, Accent.Orange200, TextShade.WHITE);
         }
     }
 }
